2cc26e76029201591a76d23c0d5aa07a5ee59e51
supermyle <kylemanley99@gmail.com>
2018-09-27 15:15:54 -0400

    First commit

---
+++ Calendar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Calendar online </title>
+		<h1> Welcome to Kyle's Online Calendar </h1>
+	</head>
+	<meta http-equiv="refresh" content="1" > 
+	<body>
+	<p> You ever wonder what time it is 
+	<i> at the current moment? </i> now you will know </p>
+	
+	<?php
+		date_default_timezone_set('America/New_York'); #Eastern Standard Time
+		$timeZone = date("e");
+		$month = date("F");
+		$day = date("l");
+		$date = date("d");
+		$dateSuff = date("S");
+		$dayYear = date("z");
+		$year = date("Y");
+		$hours = date("g");
+		$minutes = date("i");
+		$seconds = date("s");
+		
+		echo "The time zone is set to " . $timeZone . "<br>";
+		echo "Today is " . $day . "<br> " . $month .		
+		", the " . $date . $dateSuff . "<br>
+		The Year " . $year . "<br>
+		Day " . $dayYear . " of the year <br>
+		The time is " . $hours . ":" . $minutes . " and " . $seconds . " seconds";
+	?>
+	
+	</body>
+</html>
